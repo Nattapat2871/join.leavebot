8b4629199a9527496d0a26cf8294c58a17b658a1
Nattapat2871 <133991052+Nattapat2871@users.noreply.github.com>
2024-08-23 22:53:53 +0700

    Update server.py

--- server.py
+++ server.py
@@ -1,14 +1,16 @@
 from flask import Flask
 from threading import Thread
+import os
 
 app = Flask('')
 
 @app.route('/')
 def home():
-    return "server is runing!"
+    return "Server is running!"
 
 def run():
-    app.run(host='0.0.0.0',port=8080)
+    port = int(os.getenv("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
 
 def server_on():
     t = Thread(target=run)
